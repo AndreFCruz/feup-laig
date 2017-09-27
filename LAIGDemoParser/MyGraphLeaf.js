c6dfa4d10edec62fb035a51f6ccc14d3241864d4
AndreFCruz <andrecruz97@gmail.com>
2017-09-27 18:23:58 +0100

    modifications

--- LAIGDemoParser/MyGraphLeaf.js
+++ LAIGDemoParser/MyGraphLeaf.js
@@ -2,7 +2,10 @@
  * MyGraphLeaf class, representing a leaf in the scene graph.
  * @constructor
 **/
-
 function MyGraphLeaf(graph, xmlelem) {
+	var type = graph.reader.getItem(descendants[j], 'type',
+		['rectangle', 'cylinder', 'sphere', 'triangle']);
+
+	console.log("Leaf was " + leafType);
 }
 
