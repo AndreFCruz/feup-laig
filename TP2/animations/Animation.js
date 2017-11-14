50fa562cac0f0451c14472fe4d3d4c35df1ef0aa
AndreFCruz <andrecruz97@gmail.com>
2017-11-14 13:15:40 +0000

    orientation

--- TP2/animations/Animation.js
+++ TP2/animations/Animation.js
@@ -1,31 +1,28 @@
 class Animation {
 
-    constructor(id) {
-      this.id = id;
-      this.active = false;
-    }
+  constructor(id) {
+    this.id = id;
+    this.active = false;
 
-    // steps the animation delta seconds
-    step(delta) {
-      return null;
-    }
+    this.theta_ang = 0; // rotation around xx axis
+    this.phi_ang = 0; // rotation around yy axis
+  }
 
-    /** CHECK IF FUNCTIONS BELLOW THIS LINE ARE NEEDED **/
+  update(delta) {
 
-    // calculate new matrix with current parameters
-    update(delta) {
-      return null;
-    }
+  }
 
-    start() {
-      this.active = true;
-    }
+  // Converts Cartesian coordinates (from orientation 3D vector) to Spherical Coordinates
+  setOrientation(orientation) { 
+    var r = Math.sqrt( Math.pow(this.orientation[0], 2) + Math.pow(this.orientation[1], 2) + Math.pow(this.orientation[2], 2) );
 
-    stop() {
-      this.active = false;
-    }
+    this.theta_ang = Math.acos(this.orientation[1] / r) - Math.PI / 2;
+    this.phi_ang = Math.atan(this.orientation[0] / this.orientation[2]) + (this.orientation[2] < 0 ? Math.PI : 0);
+  }
 
-    isDone() {
-        return ! this.active;
-    }
+  displayOrientation(scene) {
+    scene.rotate(this.phi_ang, 0, 1, 0);
+    scene.rotate(this.theta_ang, 1, 0, 0);
+  }
+
 }
