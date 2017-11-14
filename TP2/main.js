056030f5bad980db120db292e5e42136b8d69a3c
AndreFCruz <andrecruz97@gmail.com>
2017-11-14 11:26:49 +0000

    demo_animations.xml

--- TP2/main.js
+++ TP2/main.js
@@ -35,7 +35,7 @@ main=function()
     // get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml 
     // or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor) 
     
-    var filename=getUrlVars()['file'] || "airplane.xml";
+    var filename=getUrlVars()['file'] || "demo_animations.xml";
 
     // create and load graph, and associate it to scene. 
     // Check console for loading errors
