663c795e0feaeda5de5137a31aa4b7cd81dc1d99
EdgarACarneiro <up201503784@fe.up.pt>
2017-11-14 23:30:59 +0000

    Trolha linear animation

--- TP2/animations/LinearAnimation.js
+++ TP2/animations/LinearAnimation.js
@@ -2,39 +2,54 @@ class LinearAnimation extends Animation {
   
   constructor(id, speed, controlPoints) {
     super(id);
-    
     this.speed = speed;
+
     this.controlPoints = controlPoints;
 
-    this.numCPs = this.controlPoints.length;
-    this.position = this.controlPoints[0];
-
     this.lineSegments = [];
-    this.curLineSegIdx = 0;
+    this.length = 0;
+    this.currentSegment = 0;
 
-    this.lineSegDistances = [];
-    this.runDist = 0;
+  	for (let i = 1; i < this.controlPoints.length; ++i) {
+			let segX = this.controlPoints[i][0] - this.controlPoints[i-1][0];
+			let segY = this.controlPoints[i][1] - this.controlPoints[i-1][1];
+			let segZ = this.controlPoints[i][2] - this.controlPoints[i-1][2];
+			this.lineSegments.push([segX, segY, segZ]);
 
-  	for (let i = 1; i < this.numCPs; ++i) {
-		let segX = this.controlPoints[i][0] - this.controlPoints[i-1][0];
-		let segY = this.controlPoints[i][1] - this.controlPoints[i-1][1];
-		let segZ = this.controlPoints[i][2] - this.controlPoints[i-1][2];
-		this.lineSegments.push([segX, segY, segZ]);
+			this.length += Math.sqrt(Math.pow(segX, 2) + Math.pow(segY, 2) + Math.pow(segZ, 2));
+		}
 
-		this.lineSegDistances.push(Math.sqrt(Math.pow(segX, 2) + Math.pow(segY, 2) + Math.pow(segZ, 2)));
-	}
+		this.duration = this.length / this.speed;
 
+		this.lineDivisions = [0];
+		for (let i = 0; i < this.lineSegments.length; ++i) {
+			this.lineDivisions.push(Math.sqrt(
+				Math.pow(this.lineSegments[i][0], 2) + 
+				Math.pow(this.lineSegments[i][1], 2) + 
+				Math.pow(this.lineSegments[i][2], 2)
+				)/this.length);
+		}
   }
 
-  updatePosition(xInc, yInc, zInc) {
+  update(elapsedTime) {
+  	if (elapsedTime > this.duration)
+  		return null;
+
+    let t = elapsedTime / this.duration;
+
+    let pos = this.calcPosition(t);
+    mat4.translate(this.matrix, pos);
+  }
+
+  /*updatePosition(xInc, yInc, zInc) {
   	this.position[0] += xInc;
   	this.position[1] += yInc;
   	this.position[2] += zInc;
 
   	this.runDist += Math.sqrt(Math.pow(xInc, 2) + Math.pow(yInc, 2) + Math.pow(zInc, 2));
-  }
+  }*/
 
-  checkEndOfSeg() {
+  /*checkEndOfSeg() {
   	if (this.runDist >= this.lineSegDistances[this.curLineSegIdx]) {
   		this.runDist = 0;
   		
@@ -42,20 +57,26 @@ class LinearAnimation extends Animation {
 	  		this.stop();
 	  	}
   	}
-  }
+  }*/
 
-  update(delta) {
-  	if (!this.isDone()) 
-  	{
-	  	let lineSeg = this.lineSegments[this.curLineSegIdx];
-	  	updatePosition(
-		  	lineSeg[0] * delta * this.speed,
-		  	lineSeg[1] * delta * this.speed,
-		  	lineSeg[2] * delta * this.speed
-		  );
+  calcPosition(t) {
+    if (t <= 0 || t >= 1)
+        throw new Error("Invalid t parameter to Linear Animation");
 
-	  	checkEndOfSeg();
-	  }
+		if (t > this.lineDivisions[this.currentSegment + 1]) {
+			this.currentSegment++;
+			this.setOrientation(this.lineSegments[this.currentSegment]);
+		}
+
+
+		let currSegmentPercentage = (t - this.lineDivisions[this.currentSegment]);
+		let currentSeg = this.lineSegments[this.currentSegment];
+
+		return [
+			this.controlPoint[this.currentSegment] + currentSeg[0] * currentSegment,
+			this.controlPoint[this.currentSegment] + currentSeg[1] * currentSegment,
+			this.controlPoint[this.currentSegment] + currentSeg[2] * currentSegment,
+		];
   }
 
 }
