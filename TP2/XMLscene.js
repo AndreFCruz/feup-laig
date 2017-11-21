1f1dcac1196fea8ab7f218835d593aa59401a5c0
AndreFCruz <andrecruz97@gmail.com>
2017-11-21 11:55:38 +0000

    shader works

--- TP2/XMLscene.js
+++ TP2/XMLscene.js
@@ -35,9 +35,15 @@ XMLscene.prototype.init = function(application) {
 
     this.setUpdatePeriod(1. / 60);
 
-    // TODO
-    this.shader = new CGFshader(this.gl, "shaders/selectable.vert", "shaders/selectable.frag");
-    this.setActiveShader(this.shader);
+    let selectedColor = vec4.fromValues(0.2, 1, 0.6, 0.5);
+
+    this.secondaryShader = new CGFshader(this.gl, "shaders/selectable.vert", "shaders/selectable.frag");
+    this.secondaryShader.setUniformsValues({
+      secondaryColor: selectedColor,
+      timeFactor: 0
+    });
+
+    this.setSecondaryShader();
 }
 
 /**
@@ -125,8 +131,8 @@ XMLscene.prototype.display = function() {
         // Applies initial transformations.
         this.multMatrix(this.graph.initialTransforms);
 
-		// Draw axis
-		this.axis.display();
+    // Draw axis
+    this.axis.display();
 
         var i = 0;
         for (var key in this.lightValues) {
@@ -148,11 +154,11 @@ XMLscene.prototype.display = function() {
         this.graph.displayScene();
 
     }
-	else
-	{
-		// Draw axis
-		this.axis.display();
-	}
+  else
+  {
+    // Draw axis
+    this.axis.display();
+  }
     
 
     this.popMatrix();
@@ -165,10 +171,17 @@ XMLscene.prototype.update = function(currTime) {
     this.updateShader(currTime);
 }
 
-XMLscene.prototype.updateShader = function(currTime) {
-    let t = Math.sin(currTime / 1000) + 1;
-
-    this.shader.setUniformsValues({timeFactor: t});    
+XMLscene.prototype.setDefaultShader = function() {
+    this.setActiveShader(this.defaultShader);
 }
 
+XMLscene.prototype.setSecondaryShader = function() {
+    this.setActiveShader(this.secondaryShader);
+}
+
+XMLscene.prototype.updateShader = function(currTime) {
+    let t = (Math.sin(currTime / 1000 * 2) + 1) / 4;
+
+    this.secondaryShader.setUniformsValues({timeFactor: t});    
+}
 
--- TP2/shaders/selectable.frag
+++ TP2/shaders/selectable.frag
@@ -1,4 +1,9 @@
-void main() {
-    gl_FragColor =  vec4(0.0,0.0,1, 1.0);
-}
+#ifdef GL_ES
+precision highp float;
+#endif
 
+varying vec4 vFinalColor;
+
+void main() {
+    gl_FragColor =  vFinalColor;
+}
