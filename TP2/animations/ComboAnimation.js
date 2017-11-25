37e6345a5d74716322641f54c30a18216e4a930f
AndreFCruz <andrecruz97@gmail.com>
2017-11-25 16:55:47 +0000

    nested combo animations prevented

--- TP2/animations/ComboAnimation.js
+++ TP2/animations/ComboAnimation.js
@@ -1,11 +1,19 @@
 class ComboAnimation extends Animation {
   constructor(animations) {
     super();
+    this.checkAnimationsIntegrity(animations);
     this.animations = animations;
     this.cumulative = 0;
     this.animIdx = 0;
 
     this.calcComboDuration();
+  }
+
+  checkAnimationsIntegrity(animations) {
+    for (let i = 0; i < animations.length; i++) {
+      if (animations[i] instanceof ComboAnimation)
+        throw new Error("Error: Nested combo animations.");
+    }
   }
 
   calcComboDuration() {
@@ -18,12 +26,22 @@ class ComboAnimation extends Animation {
   increaseAnimIdx() {
     if (this.animIdx + 1 < this.animations.length) {
       this.animIdx++;
+    } else {
+      this.reset();
     }
   }
 
+  reset() {
+    this.cumulative = 0;
+    this.animIdx = 0;
+    console.log("End of combo animation.");
+  }
+
   update(elapsedTime) {
-    if (elapsedTime > this.duration)
+    if (elapsedTime > this.duration) {
+      this.reset();
       return null;
+    }
 
     let anim = this.animations[this.animIdx];
     if (elapsedTime - this.cumulative > anim.duration) {
