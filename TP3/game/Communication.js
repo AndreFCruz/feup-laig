fbb50e34bf0d04639aacffa46ff7d045991ceab4
EdgarACarneiro <up201503784@fe.up.pt>
2017-12-25 17:22:11 +0000

    :santa: prologToJs :santa:

--- TP3/game/Communication.js
+++ TP3/game/Communication.js
@@ -1,15 +1,17 @@
 const COMMMUNICATION_PORT = 8081;
-
+const GET_LISTS_REGEX = /((?<=\[\[).+?(?=\|))|((?<=],\[).+?(?=\|))/g;
+const GET_ELEMENTS_REGEX = /[a-z]+(?=,)|(?<=,).+/g;
 
 function getPrologRequest(requestString)
 {
     var serverAnswer;
     var request = new XMLHttpRequest();
-    request.open('GET', 'http://localhost:'+ COMMMUNICATION_PORT +'/'+requestString, true);
+    request.open('GET', 'http://localhost:' + COMMMUNICATION_PORT + '/' + requestString, true);
 
     request.onload = function(data) {
         serverAnswer = data.target.response;
         console.log("Request successful. Reply: " + serverAnswer);
+        listsToBoard(serverAnswer);
     };
     request.onerror = function() {
         console.log("Error waiting for response");
@@ -19,4 +21,21 @@ function getPrologRequest(requestString)
     request.send();
 
     return serverAnswer;
+}
+
+function boardToLists(board) {
+
+}
+
+function listsToBoard(lists) {
+    let rows = lists.match(GET_LISTS_REGEX);
+    let board = {};
+
+    for (let i = 0; i < rows.length; ++i) {
+
+        board[i] = {};
+        board[i] = rows[i].match(GET_ELEMENTS_REGEX);
+    }
+
+    return board;
 }
