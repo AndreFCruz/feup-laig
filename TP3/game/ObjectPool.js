1fb6b4e864898114a5433ec8e8e12e00cc7bc8d1
AndreFCruz <andrecruz97@gmail.com>
2017-12-28 19:25:59 +0000

    ObjectPool added

---
+++ TP3/game/ObjectPool.js
@@ -0,0 +1,21 @@
+class ObjectPool {
+    constructor(constructorFunction, numElements) {
+        this._elements = new Array(numElements);
+
+        for (let i = 0; i < numElements; ++i) {
+            this._elements.push( constructorFunction() );
+        }
+    }
+
+    get elements() {
+        return this._elements;
+    }
+
+    acquire() {
+        return this._elements.pop();
+    }
+
+    release(object) {
+        this._elements.push(object);
+    }
+}
